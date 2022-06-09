4ec328822b09d2145f55b21ef940ac30d92a5077
Ayomideafolabi <aia0008@auburn.edu>
2022-06-09 14:07:20 -0500

    Identify and fix the errors in the following code

---
+++ Test.java
@@ -0,0 +1,15 @@
+package myjavaprograms;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double i = 50.0;
+		double k = i + 50.0;
+		double j = k + 1.0;
+		
+		System.out.println("j is " + j + " and k is " + k);
+
+	}
+
+}
